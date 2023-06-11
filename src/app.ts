4faadfafad76e6a1e3dae8ed6c43451b2d7cb8df
Shakhawathh <shakhawathh55@gmail.com>
2023-06-12 04:35:25 +0600

    error handle

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import cors from 'cors'
 import usersRouter from './app/modules/users/users.route'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 const app: Application = express()
 
 app.use(cors())
@@ -13,8 +14,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/users/', usersRouter)
 
 //Testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('working Successfully')
-})
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new ApiError(400, 'problem')
+// })
+
+//global error handler
+
+app.use(globalErrorHandler)
 
 export default app
--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -2,6 +2,7 @@ import config from '../../../config'
 import { generateUserId } from './users.utils'
 import { IUser } from './users.interface'
 import { User } from './users.model'
+import ApiError from '../../../errors/ApiError'
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   //auto generated increment id
@@ -15,7 +16,7 @@ const createUser = async (user: IUser): Promise<IUser | null> => {
   const createdUser = await User.create(user)
 
   if (!createUser) {
-    throw new Error('Failed to create user')
+    throw new ApiError(400, 'Failed to create user')
   }
   return createdUser
 }
